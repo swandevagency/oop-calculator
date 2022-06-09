764e13dd1bc6ef5525d9d538321e6a2834057bfd
Arsam <arsam5ba5@gmail.com>
2022-06-09 10:46:29 +0430

    some class added and button difined

--- function.js
+++ function.js
@@ -3,18 +3,20 @@ const numberKey = document.querySelectorAll('[data-number]')
 const OperatorKey = document.querySelectorAll('[data-operation]')
 const deleteButton = document.querySelector('.delete')
 const allClearButton = document.querySelector('.clear')
+const equal = document.querySelector('.equal')
 
 //* importing inputs 
-const previousNumber = document.querySelector('[previous-number]').innerText
+const previousNumber = document.querySelector('[previous-number]')
 const currentNumber = document.querySelector('[current-number]')
 
 //* defining variables
 let previous;
 let current;
 let result;
-let operator;
+let operation;
 
 
+// * defining class
 class calculator {
     
     constructor(previousNumber, currentNumber){
@@ -27,23 +29,68 @@ class calculator {
         }
     }
 
-    allClear(){
-        this.currentNumber = ''
-        this.previousNumber = ''
-        console.clear()
-    }
-
     del(){
-        if (this.currentNumber.length === 1) {
-            show.innerText = "";
+        if (currentNumber.length === 1) {
+            currentNumber.innerText = "";
         } else { 
-            this.currentNumber = this.currentNumber.substring(0, this.currentNumber.length - 1)
+            currentNumber.innerText = currentNumber.innerText.substring(0, currentNumber.innerText.length - 1)
         }
     }
 
-    operation(){
+    allClear(){
+        let clearMonitor = document.querySelector('.current-number') 
+        document.querySelector('.current-number').innerHTML = '';  
+        document.querySelector('.previous-number').innerHTML = '';
+        console.clear()
+    }
+
+    numbers(){
+        const x = event.target.innerText;
+        console.log('Number',x)
+        return currentNumber.innerText += x;
+    }
+
+   
+    calculate(){
+        previous = parseFloat(currentNumber.innerText) 
+        current = parseFloat(previousNumber.innerText)
+        
+
+        switch(operation) {
+
+            case '+':
+                result = current + previous;
+                console.log(`${previous} + ${current} = ${result}`);
+                break;
+      
+            case '-':
+                result = current - previous;
+                console.log(`${previous} - ${current} = ${result}`);
+                break;
+      
+            case '*':
+                result = current * previous;
+                console.log(`${previous} * ${current} = ${result}`);
+                break;
+      
+            case '/':
+                if(current == 0){
+                    return currentNumber.innerText = "Error!";
+                }
+                result = current / previous;
+                console.log(`${previous} / ${current} = ${result}`);
+                break;
+    
+            default:
+                break
+        }
+        currentNumber.innerText = result;
+        previousNumber.innerText = '';
+    }
+
+    operate(){
         if(currentNumber.innerText && previousNumber.innerText) {
-            calculated()
+            this.calculate()
         }
         operation = event.target.innerText;
         currentNumber.innerText += operation
@@ -55,46 +102,47 @@ class calculator {
     
     }
 
-    calculate(){
-        previous = parseFloat(this.currentNumber) 
-        current = parseFloat(this.previousNumber)
-        
-
-        switch(operation) {
-
-            case '+':
-                result = previous + current;
-                console.log(`${previous} + ${current} = ${result}`);
-                break;
-      
-            case '-':
-                result = previous - current;
-                console.log(`${previous} - ${current} = ${result}`);
-                break;
-      
-            case '*':
-                result = previous * current;
-                console.log(`${previous} * ${current} = ${result}`);
-                break;
-      
-            case '/':
-                if(current == 0){
-                    return currentNumber.innerText = "Error!";
-                }
-                result = previous / current;
-                console.log(`${previous} / ${current} = ${result}`);
-                break;
-    
-            default:
-                break
-        }
-        this.currentNumber = result;
-        this.previousNumber = '';
-    }
 
 }
 
+calculator = new calculator(
+    previousNumber,
+    currentNumber
+)
 
+// * defining buttons
+
+deleteButton.addEventListener('click', () => {
+    calculator.del()
+})
+
+allClearButton.addEventListener("click", () => {
+     calculator.allClear()
+});
+
+numberKey.forEach(button => {
+    button.addEventListener('click', (event) => {
+        calculator.numbers()        
+    })
+})
+
+OperatorKey.forEach(butOperation => {
+    butOperation.addEventListener('click', () => {
+        calculator.operate()        
+    })    
+});
+
+equal.addEventListener('click', () => {
+    calculator.calculate()
+})
+
+function refreshPage(){
+    if (confirm('Are you sure you want to reload this page?')){
+        window.location.reload();
+    }else{
+        //do nothing
+    }
+} 
 
 
 
